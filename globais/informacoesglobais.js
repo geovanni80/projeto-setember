cae1522ba4ab28d5325d9094261685bffb4cb822
geovanni80 <133605866+geovanni80@users.noreply.github.com>
2024-10-02 10:45:24 -0300

    Add files via upload

--- globais/informacoesglobais.js
+++ globais/informacoesglobais.js
@@ -4,6 +4,9 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(url)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem`
 }
 
 vizualizarInformacoesGlobais()
