a5e02cefb22b9976b4d24ef3138ed3bfafbaeec5
geovanni80 <133605866+geovanni80@users.noreply.github.com>
2024-10-16 10:49:09 -0300

    Add files via upload

--- globais/informacoesglobais.js
+++ globais/informacoesglobais.js
@@ -6,7 +6,11 @@ async function vizualizarInformacoesGlobais() {
     console.log(url)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem`
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
